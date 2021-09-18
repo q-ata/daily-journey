836618cc5c20f15779cfafbe456ba9f0edb6c588
Raymond <zhangraymond05@gmail.com>
2021-09-18 19:56:06 -0400

    Bug fix.

--- htn/app/views.py
+++ htn/app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
 from django.urls import reverse
+import rest_framework
 from rest_framework.response import Response
 from .models import User, RunHistory
 from .serializers import LoginSerializer, RegisterSerializer, RunHistorySerializer
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, views
 from .mapper import Mapper
 import json
 
@@ -30,7 +31,7 @@ class LoginView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = LoginSerializer
 
-class MapView(generics.CreateAPIView):
+class MapView(views.APIView):
     def post(self, request):
         m = Mapper()
         res = m.getMap()
