ae65915e86017a725782f6e06f1ea1e65b9d49fb
Raymond <zhangraymond05@gmail.com>
2021-09-18 20:07:50 -0400

    Deleting unnecessary imports.

--- htn/app/views.py
+++ htn/app/views.py
@@ -1,15 +1,12 @@
-from htn.app import getbestroutes
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
 from django.urls import reverse
-import rest_framework
 from rest_framework.response import Response
 from .models import User, RunHistory
 from .serializers import LoginSerializer, RegisterSerializer, RunHistorySerializer
 from rest_framework import viewsets, generics, views
 from .mapper import Mapper
 from .getbestroutes import RouteFinder
-import json
 
 # Create your views here.
 
