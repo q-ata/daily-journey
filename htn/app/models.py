26f67b2e9d6975a1b94fda198f3863bb98eae16f
Raymond <zhangraymond05@gmail.com>
2021-09-19 02:58:41 -0400

    database fixes.

--- htn/app/models.py
+++ htn/app/models.py
@@ -26,6 +26,7 @@ class StartingLocs(models.Model):
 
 class SavedPaths(models.Model):
     userid = models.ForeignKey(User, on_delete=models.CASCADE)
+    pathid = models.IntegerField(null=True)
     pathpoints = models.TextField()
 
 #model here
